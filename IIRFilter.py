910c32dc533e6f75b1d5947292d87a585f0130e0
EricoDeMecha <techcider4134@gmail.com>
2020-12-06 16:37:39 +0300

    wrap up

--- IIRFilter.py
+++ IIRFilter.py
@@ -8,6 +8,11 @@ class IIRFilter:
         # pass the elements of the sos to the constructor of each.
 
     def filter(self, x):
-        result = [obj.filter(x) for obj in self.IIR]  # pass the value x through  the chain of IIR2Filters while
+        # pass the value x through  the chain of IIR2Filters while
         # calling the filter function.
-        return sum(result)/len(result)
+        result = list() # list to store the result
+        for obj in self.IIR:
+            rvalue = obj.filter(x)
+            result = rvalue
+            x = rvalue
+        return sum(result)/len(result) # the average of the list.
--- runSetup.py
+++ runSetup.py
@@ -2,6 +2,7 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import pyfirmata
+from IIRFilter import IIRFilter
 
 # Create figure for plotting
 fig = plt.figure()
@@ -17,13 +18,30 @@ it.start()
 
 ldr = board.analog[0]
 ldr.enable_reporting()
+# Filter init
+# Generating sos coefficients
+'''
+order: 2
+cuttoff: [45,55]
+filter type: 'bandstop'
+design: cheby1
+rp:0.01
+rs: 1
+fs: 200
+'''
+sos = [2, [45, 55], 'bandstop', 'cheby1', 0.01, 1, 200]
+
+# 2. setup filters
+IIRFilter(sos)  # filters are setup with coefficients
 
 
 # This function is called periodically from FuncAnimation
 def animate(i, xs, ys):
+    # get the output of the sensor
+    ldr_val = ldr.read()
     # Add x and y to lists
     xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
-    ys.append(ldr.read()*260000) # 130000 lux is the maximum light intensity in a green house.
+    ys.append(ldr_val * 260000)  # 130000 lux is the maximum light intensity in a green house.
 
     # Limit x and y lists to 20 items
     xs = xs[-20:]
@@ -33,8 +51,8 @@ def animate(i, xs, ys):
     ax.clear()
     ax.plot(xs, ys, 'b')
     # apply filter
-    # 1. Generating sos coefficients
-    sos =
+    filter_ys = IIRFilter.filter(ldr_val)
+    ax.plot(xs, ys, 'r')  # plot the result
     # Format plot
     plt.xticks(rotation=45, ha='right')
     plt.subplots_adjust(bottom=0.30)
